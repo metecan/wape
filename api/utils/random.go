c1be9cd8d0dbf138a994153b34f4afce01a6c392
Metecan Kaplan <xhild0101@gmail.com>
2022-06-02 23:51:08 +0300

    Created random generator

---
+++ api/utils/random.go
@@ -0,0 +1,13 @@
+package utils
+
+import "math/rand"
+
+func GenerateRandom() string {
+	letters := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+	b := make([]rune, 8)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+
+	return string(b)
+}
